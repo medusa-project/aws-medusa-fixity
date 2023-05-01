5fa014189c9d14cd754f22e61802644ea64a6661
Gen Schmitt <gschmitt@illinois.edu>
2023-05-01 09:58:41 -0600

    updated bin scripts

--- bin/fetch_responses.sh
+++ bin/fetch_responses.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --login
 #fetch responses from the S3 restoration events
 
-( cd /Users/gschmitt/workspace/fixity || exit; ruby -r "./lib/restoration_event.rb" -e "RestorationEvent.handle_message" )
+( cd /Users/gschmitt/workspace/aws-medusa-fixity || exit; ruby -r "./lib/restoration_event.rb" -e "RestorationEvent.handle_message" )
 
 exit 0
--- bin/initiate_batch_restoration.sh
+++ bin/initiate_batch_restoration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --login
 #Run fixity on restored files in order of restoration completion
 
-( cd /Users/gschmitt/workspace/fixity || exit; ruby -r "./lib/restore_files.rb" -e "RestoreFiles.get_batch" )
+( cd /Users/gschmitt/workspace/aws-medusa-fixity || exit; ruby -r "./lib/restore_files.rb" -e "RestoreFiles.get_batch" )
 
 exit 0
