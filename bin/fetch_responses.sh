ab5b5b4c8da8050c31b5f97c3fe9259eb0e15f7b
Gen Schmitt <gschmitt@illinois.edu>
2023-05-02 13:19:44 -0600

    updated bin scripts

--- bin/fetch_responses.sh
+++ bin/fetch_responses.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --login
 #fetch responses from the S3 restoration events
 
-( cd /Users/gschmitt/workspace/aws-medusa-fixity || exit; ruby -r "./lib/restoration_event.rb" -e "RestorationEvent.handle_message" )
+( cd /home/ec2-user/aws-medusa-fixity || exit; ruby -r "./lib/restoration_event.rb" -e "RestorationEvent.handle_message" )
 
 exit 0
