992fdb924a962d273d4c8bbf14d54c2aeb927911
Gen Schmitt <gschmitt@illinois.edu>
2023-05-16 13:25:05 -0600

    updated scripts to scale down for testing

--- bin/compute_fixity.sh
+++ bin/compute_fixity.sh
@@ -1,9 +1,10 @@
 #!/bin/bash --login
 #Run fixity on restored files in order of restoration completion, run fixity every 5 seconds
-for  (( i=1; i <= 12; i++ ))
-do
-    ( cd /home/ec2-user/aws-medusa-fixity || exit; ruby -r "./lib/fixity.rb" -e "Fixity.run_fixity" & )
-    sleep 5
-done
+#for  (( i=1; i <= 12; i++ ))
+#do
+#    ( cd /home/ec2-user/aws-medusa-fixity || exit; ruby -r "./lib/fixity.rb" -e "Fixity.run_fixity" & )
+#    sleep 5
+#done
 
+( cd /home/ec2-user/aws-medusa-fixity || exit; ruby -r "./lib/fixity.rb" -e "Fixity.run_fixity" )
 exit 0
