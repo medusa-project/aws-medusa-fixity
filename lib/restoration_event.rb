0056d77868018f23b144dea8ca0c6a957367aea5
Gen Schmitt <gschmitt@illinois.edu>
2023-05-16 13:55:09 -0600

    updated for testing

--- lib/restoration_event.rb
+++ lib/restoration_event.rb
@@ -83,13 +83,14 @@ class RestorationEvent
           file_id = update_item_resp.attributes[FixityConstants::FILE_ID]
           initial_checksum = update_item_resp.attributes[FixityConstants::INITIAL_CHECKSUM]
           error_message = "File #{file_id} expired before being processed by fixity"
+          FixityConstants::LOGGER.error(update_item_resp.attributes)
           FixityConstants::LOGGER.error(error_message)
           countFile = File.open("expirationCount.txt")
           count= countFile.read.to_i
           countFile.close
           File.write("expirationCount.txt", count+1)
           item = MedusaItem.new(s3_key, file_id, initial_checksum)
-          RestoreFiles.restore_batch([item])
+          # RestoreFiles.restore_batch([item])
         end
       else
         error_message = "Unknown restore type #{restore_type}"
