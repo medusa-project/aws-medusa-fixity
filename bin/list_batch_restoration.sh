f16ef5f48b30136af275f553922d80e59b1ad82b
Gen Schmitt <gschmitt@illinois.edu>
2023-07-17 13:17:21 -0600

    updated list restoration

--- bin/list_batch_restoration.sh
+++ bin/list_batch_restoration.sh
@@ -2,6 +2,6 @@
 source /home/ec2-user/aws-medusa-fixity/bin/set-vars.sh
 #Run fixity on restored files in order of restoration completion
 
-( cd /home/ec2-user/aws-medusa-fixity || exit; ruby -r "./lib/batch_restore_files.rb" -e "BatchRestoreFiles.get_batch_from_list ['120479', '61138', '663727', '655861', '639857', '69886', '632374', '68656', '50174', '647025', '587873', '69797', '125132', '50633', '50024']" )
+( cd /home/ec2-user/aws-medusa-fixity || exit; ruby -r "./lib/batch_restore_files.rb" -e "BatchRestoreFiles.get_batch_from_list ['120479', '69837', '69797', '78804', '75896']" )
 
 exit 0
