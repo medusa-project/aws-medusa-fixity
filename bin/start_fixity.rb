f668065052236cbc95a7df7f41602bc7b8ce3abc
Gen Schmitt <gschmitt@illinois.edu>
2023-07-31 14:36:41 -0600

    removed test log statements

--- bin/start_fixity.rb
+++ bin/start_fixity.rb
@@ -11,9 +11,7 @@ bin_home = ENV['BIN_HOME']
 pid_files = %W[#{temp_home}/fixity.1.pid #{temp_home}/fixity.2.pid #{temp_home}/fixity.3.pid]
 
 pid_files.each do |pid_file|
-  pid_id = File.read(pid_file).split.first.to_i
   running = Pid.running?(File.read(pid_file).split.first.to_i)
-  FixityConstants::LOGGER.info("Pid #{pid_id} running?: #{running}")
   next if running
 
   task = IO.popen("ruby #{bin_home}/compute_fixity.rb")
