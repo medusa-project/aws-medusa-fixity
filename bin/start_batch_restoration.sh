b0239ffb565a7bc29e00126352287d001358fbac
Gen Schmitt <gschmitt@illinois.edu>
2023-07-31 09:47:56 -0600

    updated bin script file name

--- bin/start_batch_restoration.sh
+++ bin/start_batch_restoration.sh
@@ -2,7 +2,7 @@
 source /home/ec2-user/aws-medusa-fixity/bin/set-vars.sh
 
 #Run fixity on restored files in order of restoration completion
-ruby "$BIN_HOME"/start_batch_restoration.rb &
+ruby "$BIN_HOME"/initiate_batch_restoration.rb &
 RESTORATION_PID=$!
 echo "$RESTORATION_PID" > "$TMP_HOME"/batch_restoration.pid
 exit 0
