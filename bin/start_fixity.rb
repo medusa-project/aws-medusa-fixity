ee7a5601e263b0634a6c975b596ccdae5b8c92cd
Gen Schmitt <gschmitt@illinois.edu>
2023-08-04 14:47:09 -0600

    added fixity process

--- bin/start_fixity.rb
+++ bin/start_fixity.rb
@@ -6,7 +6,7 @@ require_relative '../lib/fixity/fixity_constants'
 
 temp_home = ENV['TMP_HOME']
 bin_home = ENV['BIN_HOME']
-pid_files = %W[#{temp_home}/fixity.1.pid #{temp_home}/fixity.2.pid #{temp_home}/fixity.3.pid]
+pid_files = %W[#{temp_home}/fixity.1.pid #{temp_home}/fixity.2.pid #{temp_home}/fixity.3.pid #{temp_home}/fixity.4.pid]
 
 pid_files.each do |pid_file|
   running = Pid.running?(File.read(pid_file).split.first.to_i)
