66c7fe1562863c771a5424c1a501c93539480747
Gen Schmitt <gschmitt@illinois.edu>
2023-08-04 14:53:37 -0600

    updated bin script

--- bin/start_list_batch_restoration.sh
+++ bin/start_list_batch_restoration.sh
@@ -2,7 +2,7 @@
 source /home/ec2-user/aws-medusa-fixity/bin/set-vars.sh
 #Batch restore files from a list of file ids
 
-ruby "$BIN_HOME"/process_batch_reports.rb &
+ruby "$BIN_HOME"/list_batch_restoration.rb &
 RESTORATION_PID=$!
 echo "$RESTORATION_PID" > "$TMP_HOME"/list_batch_restoration.pid
 
