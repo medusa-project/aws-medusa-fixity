e61730ff6b00a80aba278adbd5a1623e1e3513ba
Gen Schmitt <gschmitt@illinois.edu>
2023-08-07 08:51:54 -0600

    updated to run fixity perpetually

--- bin/start_fixity.sh
+++ bin/start_fixity.sh
@@ -3,11 +3,11 @@ source /home/ec2-user/aws-medusa-fixity/bin/set-vars.sh
 
 FIXITY_PID=$$
 echo "$FIXITY_PID" > "$TMP_HOME"/start_fixity.pid
-#Run fixity on restored files in order of restoration completion, run fixity every 5 seconds
-for  (( i=1; i <= 6; i++ ))
+#Run fixity on restored files in order of restoration completion
+while :
 do
-    ruby "$BIN_HOME"/start_fixity.rb &
-    sleep 10
+    # test in foreground and in background
+    ruby "$BIN_HOME"/start_fixity.rb
 done
 
 exit 0
