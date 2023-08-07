06fd240a916ac1f0e66b9854a79a15f34058e9d6
Gen Schmitt <gschmitt@illinois.edu>
2023-08-07 10:06:30 -0600

    added fixity script for monit

--- bin/start_fixity.sh
+++ bin/start_fixity.sh
@@ -2,7 +2,7 @@
 source /home/ec2-user/aws-medusa-fixity/bin/set-vars.sh
 
 FIXITY_PID=$$
-echo "$FIXITY_PID" > "$TMP_HOME"/start_fixity.pid
+#echo "$FIXITY_PID" > "$TMP_HOME"/start_fixity.pid
 #Run fixity on restored files in order of restoration completion
 while :
 do
