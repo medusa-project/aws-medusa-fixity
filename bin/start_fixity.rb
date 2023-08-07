34961d0900923e024a50daf1e84e9e5ab74db77e
Gen Schmitt <gschmitt@illinois.edu>
2023-08-07 08:36:56 -0600

    updated bin scripts for running fixity

--- bin/start_fixity.rb
+++ bin/start_fixity.rb
@@ -6,7 +6,8 @@ require_relative '../lib/fixity/fixity_constants'
 
 temp_home = ENV['TMP_HOME']
 bin_home = ENV['BIN_HOME']
-pid_files = %W[#{temp_home}/fixity.1.pid #{temp_home}/fixity.2.pid #{temp_home}/fixity.3.pid #{temp_home}/fixity.4.pid]
+pid_files = %W[#{temp_home}/fixity.1.pid #{temp_home}/fixity.2.pid #{temp_home}/fixity.3.pid #{temp_home}/fixity.4.pid
+               #{temp_home}/fixity.5.pid #{temp_home}/fixity.6.pid #{temp_home}/fixity.7.pid #{temp_home}/fixity.8.pid]
 
 pid_files.each do |pid_file|
   running = Pid.running?(File.read(pid_file).split.first.to_i)
--- bin/start_fixity.sh
+++ bin/start_fixity.sh
@@ -1,6 +1,8 @@
 #!/bin/bash --login
 source /home/ec2-user/aws-medusa-fixity/bin/set-vars.sh
 
+FIXITY_PID=$$
+echo "FIXITY_PID" > "$TMP_HOME"/start_fixity.pid
 #Run fixity on restored files in order of restoration completion, run fixity every 5 seconds
 for  (( i=1; i <= 6; i++ ))
 do
