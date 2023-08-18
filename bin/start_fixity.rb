82219495094edce25f96116ddf9c630aa68aaeba
Gen Schmitt <gschmitt@illinois.edu>
2023-08-18 09:10:27 -0600

    pause fixity calculations when files are batch restoration is running

--- bin/start_fixity.rb
+++ bin/start_fixity.rb
@@ -6,8 +6,12 @@ require_relative '../lib/fixity/fixity_constants'
 
 temp_home = ENV['TMP_HOME']
 bin_home = ENV['BIN_HOME']
+
 pid_files = %W[#{temp_home}/fixity.1.pid #{temp_home}/fixity.2.pid #{temp_home}/fixity.3.pid #{temp_home}/fixity.4.pid
                #{temp_home}/fixity.5.pid #{temp_home}/fixity.6.pid #{temp_home}/fixity.7.pid #{temp_home}/fixity.8.pid]
+batch_restore_pid = "#{temp_home}/batch_restoration.pid"
+batch_restoration_running = Pid.running?(File.read(batch_restore_pid))
+sleep 60 and exit if batch_restoration_running
 
 pid_files.each do |pid_file|
   running = Pid.running?(File.read(pid_file).split.first.to_i)
