fbf630d5f8ad6a4721799d37ce093b992c7045e6
Gen Schmitt <gschmitt@illinois.edu>
2023-08-28 08:16:26 -0600

    in progres local update

--- bin/set-vars.sh
+++ bin/set-vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export RUBY_ENV=local
-export RUBY_HOME=/home/ec2-user/aws-medusa-fixity
+export RUBY_HOME=/Users/gschmitt/workspace/aws-medusa-fixity
 export TEST_HOME=$RUBY_HOME/test
 export BIN_HOME=$RUBY_HOME/bin
 export TMP_HOME=$RUBY_HOME/tmp
