4e5fdecc40a413f592e157be4972534f80595269
Gen Schmitt <gschmitt@illinois.edu>
2023-09-25 14:20:08 -0600

    increasing the number of sqs messages processed

--- bin/start_fetch_responses.sh
+++ bin/start_fetch_responses.sh
@@ -2,12 +2,12 @@
 source /home/ec2-user/aws-medusa-fixity/bin/set-vars.sh
 
 #fetch responses from the S3 restoration events, check every second
-for  (( i=1; i <= 15; i++ ))
+for  (( i=1; i <= 20; i++ ))
 do
     ruby "$BIN_HOME"/fetch_responses.rb &
     RESPONSE_PID=$!
     echo "$RESPONSE_PID" > "$TMP_HOME"/fetch_responses."${i}".pid
-    sleep 4
+    sleep 3
 done
 
 exit 0
