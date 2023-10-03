8b49004f0fdb6a3e2303ce0eb6f80a7b367d93a2
Gen Schmitt <gschmitt@illinois.edu>
2023-10-03 14:01:54 -0600

    added docker support for github actions testing

--- lib/fixity/fixity_constants.rb
+++ lib/fixity/fixity_constants.rb
@@ -8,11 +8,12 @@ require 'config'
 
 class FixityConstants
   Config.load_and_set_settings(Config.setting_files("#{ENV['RUBY_HOME']}/config", ENV['RUBY_ENV']))
+
   # AWS
   S3_CLIENT = Aws::S3::Client.new(region: Settings.aws.region_west)
   SQS_CLIENT_WEST = Aws::SQS::Client.new(region: Settings.aws.region_west)
   SQS_CLIENT_EAST = Aws::SQS::Client.new(region: Settings.aws.region_east)
   DYNAMODB_CLIENT = Aws::DynamoDB::Client.new(region: Settings.aws.region_west)
   LOGGER = Logger.new("#{ENV['LOGGER_HOME']}/fixity.log", 'daily')
-  S3_CONTROL_CLIENT = Aws::S3Control::Client.new(region: Settings.aws.region_west)
+  S3_CONTROL_CLIENT = ENV['RUBY_ENV'] != 'test' ? Aws::S3Control::Client.new(region: Settings.aws.region_west) : ""
 end
