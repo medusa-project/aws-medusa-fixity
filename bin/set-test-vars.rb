4edb600992acde58d66f982aa059b632bde41eae
Gen Schmitt <gschmitt@illinois.edu>
2023-10-04 09:26:27 -0600

    added github action

--- bin/set-test-vars.rb
+++ bin/set-test-vars.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-ENV['RUBY_ENV']='test'
-ENV['RUBY_HOME']='/Users/gschmitt/workspace/aws-medusa-fixity'
-ENV['TEST_HOME']="#{ENV['RUBY_HOME']}/test"
-ENV['LOGGER_HOME']="#{ENV['RUBY_HOME']}/logs"
+ENV['RUBY_ENV'] = 'test'
+ENV['RUBY_HOME'] = ENV['IS_DOCKER'] == 'true' ? '/fixity' : '/Users/gschmitt/workspace/aws-medusa-fixity'
+ENV['TEST_HOME'] = "#{ENV['RUBY_HOME']}/test"
+ENV['LOGGER_HOME'] = "#{ENV['RUBY_HOME']}/logs"
---
+++ lib/fixity/fixity_secrets.rb.template
@@ -0,0 +1,7 @@
+class FixitySecrets
+  MEDUSA_DB_HOST = 'host'
+  MEDUSA_DB_PASSWORD = 'somethingClever'
+  MEDUSA_DB_USER = 'user'
+  MEDUSA_DB_NAME = 'name'
+  MEDUSA_DB = "db"
+end
