cacf0644a67e763a329a5c647fbf33c7258a324f
Gen Schmitt <gschmitt@illinois.edu>
2023-10-30 09:52:08 -0600

    increased message handling for now

--- bin/start_fetch_responses.sh
+++ bin/start_fetch_responses.sh
@@ -2,12 +2,12 @@
 source /home/ec2-user/aws-medusa-fixity/bin/set-vars.sh
 
 #fetch responses from the S3 restoration events, check every second
-for  (( i=1; i <= 30; i++ ))
+for  (( i=1; i <= 60; i++ ))
 do
     ruby "$BIN_HOME"/fetch_responses.rb &
     RESPONSE_PID=$!
     echo "$RESPONSE_PID" > "$TMP_HOME"/fetch_responses."${i}".pid
-    sleep 2
+    sleep 1
 done
 
 exit 0
