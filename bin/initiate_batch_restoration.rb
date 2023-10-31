4fd0f16f6a5566421741dc64a4e43c7d312dc0fa
Gen Schmitt <gschmitt@illinois.edu>
2023-10-31 13:04:25 -0600

    updated initiate_batch_restoration to process expired files

--- bin/initiate_batch_restoration.rb
+++ bin/initiate_batch_restoration.rb
@@ -3,5 +3,8 @@
 
 require_relative '../lib/batch_restore_files'
 
+manifest = 'manifest-expired-2023-10-26-00:00.csv' # manifest-2023-10-27-00:00.csv manifest-2023-10-28-00:00.csv
 batch_restoration = BatchRestoreFiles.new
-batch_restoration.batch_restore
+# batch_restoration.batch_restore
+etag = batch_restoration.put_manifest(manifest)
+send_batch_job(manifest, etag)
