92808290425f1e6b05b7bcef63d2116ca1a7543b
Gen Schmitt <gschmitt@illinois.edu>
2023-10-31 12:59:40 -0600

    removed method to handle expired files

--- lib/fixity/fixity_utils.rb
+++ lib/fixity/fixity_utils.rb
@@ -11,22 +11,4 @@ class FixityUtils
   def self.unescape(s3_key)
     CGI.unescape(s3_key)
   end
-
-  def self.compareCSV(expired, manifest, new_manifest)
-    manifest_csv = CSV.new(File.read(manifest))
-    manifest_keys = []
-    manifest_csv.each do |row|
-      _manifest_bucket, manifest_key = row
-      manifest_keys.push(manifest_key)
-    end
-    expired_csv = CSV.new(File.read(expired))
-    expired_csv.each do |row|
-      expired_bucket, expired_key = row
-      next unless manifest_keys.include?(expired_key)
-
-      open(new_manifest, 'a') { |f|
-        f.puts "#{expired_bucket},#{expired_key}"
-      }
-    end
-  end
 end
