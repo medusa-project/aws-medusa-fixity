5cd62c802ee400e9d6653587fae8a904765b10c6
Gen Schmitt <gschmitt@illinois.edu>
2023-11-03 14:44:25 -0600

    updated to restore new files

--- bin/initiate_batch_restoration.rb
+++ bin/initiate_batch_restoration.rb
@@ -3,8 +3,5 @@
 
 require_relative '../lib/batch_restore_files'
 
-manifest = 'manifest-2023-10-28-00:00.csv'
 batch_restoration = BatchRestoreFiles.new
-# batch_restoration.batch_restore
-etag = batch_restoration.put_manifest(manifest)
-batch_restoration.send_batch_job(manifest, etag)
+batch_restoration.batch_restore
