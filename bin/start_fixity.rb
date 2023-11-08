bc91429d067e671fdc148a4f3774dc7c87a8e815
Gen Schmitt <gschmitt@illinois.edu>
2023-11-08 09:06:44 -0700

    increase fixity process to 9

--- bin/start_fixity.rb
+++ bin/start_fixity.rb
@@ -8,8 +8,8 @@ temp_home = ENV['TMP_HOME']
 bin_home = ENV['BIN_HOME']
 
 pid_files = %W[#{temp_home}/fixity.1.pid #{temp_home}/fixity.2.pid #{temp_home}/fixity.3.pid #{temp_home}/fixity.4.pid
-               #{temp_home}/fixity.5.pid #{temp_home}/fixity.6.pid #{temp_home}/fixity.7.pid #{temp_home}/fixity.8.pid]
-#               #{temp_home}/fixity.9.pid #{temp_home}/fixity.10.pid #{temp_home}/fixity.11.pid #{temp_home}/fixity.12.pid]
+               #{temp_home}/fixity.5.pid #{temp_home}/fixity.6.pid #{temp_home}/fixity.7.pid #{temp_home}/fixity.8.pid
+               #{temp_home}/fixity.9.pid] # #{temp_home}/fixity.10.pid #{temp_home}/fixity.11.pid #{temp_home}/fixity.12.pid]
 #              #{temp_home}/fixity.13.pid #{temp_home}/fixity.14.pid #{temp_home}/fixity.15.pid #{temp_home}/fixity.16.pid]
 batch_restore_pid = "#{temp_home}/batch_restoration.pid"
 batch_restoration_running = Pid.running?(File.read(batch_restore_pid).split.first.to_i)
